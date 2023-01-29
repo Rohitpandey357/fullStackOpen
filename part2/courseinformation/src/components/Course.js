6c4220b08cf105ccae47d3c5d3363aaed7653898
Rohit Pandey <rohitpandey357@gmail.com>
2023-01-29 12:57:23 +0530

    Completed exercise 2.2

--- part2/courseinformation/src/components/Course.js
+++ part2/courseinformation/src/components/Course.js
@@ -1,6 +1,6 @@
 import Header from './Header';
 import Content from './Content';
-
+import Total from './Total';
 
 const Course = (props) => {
     // console.log(props);
@@ -9,6 +9,7 @@ const Course = (props) => {
         <div>
             <Header name = {course.name} />
             <Content parts = {course.parts.map(part => part)} />
+            <Total exercises = {course.parts.map(part => part.exercises)} />
         </div>
     )
 }
--- part2/courseinformation/src/components/Total.js
+++ part2/courseinformation/src/components/Total.js
@@ -1,7 +1,9 @@
 const Total = (props) => {
     // console.log(props);
+    const exercises = props.exercises;
+
     return (
-      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+      <p>Total number of exercises {exercises.reduce((a, b) => a + b, 0)} </p>
     )
 }
 
